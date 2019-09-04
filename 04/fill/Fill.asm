cfca1196caa99cdd6245c871f81ef6059ea88e3d
todoroff <stoyansky@gmail.com>
2019-09-04 07:55:10 +0100

    completed Fill program

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,115 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+@SCREEN
+D = A
+@addr
+M = D //addr = 16384 (screen's base address)
+
+@flag //default = 0; set to 1 if screen was filled in previous iteration
+	  //to reduce memory read/write
+M = 0
+
+@i
+M = 0
+
+@8192
+D = A
+@n
+M = D // n = 8192 (number of 16bit words that make up the whole screen)
+
+(MAINLOOP)
+	@KBD
+	D = M
+
+	@FILL
+	D;JNE //if key pressed goto FILL
+
+	@UNFILL
+	D;JEQ //if no key is pressed goto UNFILL
+	
+	(FILL)
+		@flag
+		D = M
+		@MAINLOOP
+		D;JNE //if flag is up return to MAINLOOP
+
+		(FILLLOOP)
+			@i
+			D = M
+			@n
+			D = D-M
+			@EXITFIL
+			D;JEQ //if i==n goto 70
+
+			@addr
+			A = M
+			M = -1 //fill
+
+			@addr
+			M = M + 1
+			@i
+			M = M + 1
+
+			@FILLLOOP
+			0;JMP
+
+
+		(EXITFIL)
+			@flag
+			M = 1 //set flag
+			
+			@i
+			M = 0 //reset i
+			
+			@SCREEN
+			D = A
+			@addr
+			M = D // reset addr
+
+
+			@MAINLOOP
+			0;JMP //return to MAINLOOP
+
+	(UNFILL)
+		@flag
+		D = M
+		@MAINLOOP
+		D;JEQ //if flag isn't up return to MAINLOOP
+
+		(UNFILLLOOP)
+			@i
+			D = M
+			@n
+			D = D-M
+			@EXITUNFIL
+			D;JEQ //if i==n goto 70
+
+			@addr
+			A = M
+			M = 0 //unfill
+
+			@addr
+			M = M + 1
+			@i
+			M = M + 1
+
+			@UNFILLLOOP
+			0;JMP
+
+
+		(EXITUNFIL)
+			@flag
+			M = 0 //set flag
+			
+			@i
+			M = 0 //reset i
+			
+			@SCREEN
+			D = A
+			@addr
+			M = D // reset addr
+
+
+			@MAINLOOP
+			0;JMP //return to MAINLOOP
